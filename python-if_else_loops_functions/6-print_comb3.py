534b11947813678abed1625b28211d37f511b51b
NourKasmi <nourkasmi@gmail.com>
2025-01-18 15:22:27 +0100

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
         if num1 == 8 and num2 == 9:
-            print("{:d}{:d}".format(num1, num2))
+            print("{}{}".format(num1, num2))
         else:
-            print("{:d}{:d}".format(num1, num2), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
