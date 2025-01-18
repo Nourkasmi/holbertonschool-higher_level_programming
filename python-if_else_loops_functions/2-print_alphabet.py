c619ee623de55da5e0a6a83b5e7e3a1f8519631a
NourKasmi <nourkasmi@gmail.com>
2025-01-18 14:47:01 +0100

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
 
-l_digit = abs(number) % 10
-
-if number < 0:
-    l_digit = -l_digit
-
-print(f"Last digit of {number} is {l_digit}", end=" ")
-
-if l_digit > 5:
-    print("and is greater than 5")
-elif l_digit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+for i in range(97, 123):
+    print(chr(i), end="")
