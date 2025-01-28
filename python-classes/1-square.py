11fe3e84aaf660cd6570a26cb89a2caaea9fe680
NourKasmi <nourkasmi@gmail.com>
2025-01-28 12:00:35 +0100

    documentation2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""Defines a class Square with a private instance attribute size."""
+
 
 class Square:
-    """A class that defines a square with a private size attribute."""
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
     def __init__(self, size):
-        """
-        Initializes a new Square.
-        
+        """Initializes a new Square instance.
+
         Args:
             size (int): The size of the square.
         """
         self.__size = size
+
