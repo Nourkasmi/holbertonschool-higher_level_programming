7ab8733d0a2c2c8c5d39db827d113de3d8850685
NourKasmi <nourkasmi@gmail.com>
2025-02-07 19:07:39 +0100

    raise

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry empty class."""
+    pass
+def area(self):
+    """Raise an exception."""
+    raise exception("area() is not implemented")
