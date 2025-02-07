0586da5ff16b690153bbc40ce5e38826ec6163c4
NourKasmi <nourkasmi@gmail.com>
2025-02-07 18:56:10 +0100

    documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """
-Module that defines the function is_kind_of_class.
+Module for is_kind_of_class function.
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """
-    Returns True if obj is an instance of, or an instance of a class
-    that inherited from, the specified class; otherwise False.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-
-    Returns:
-        bool: True if obj is an instance, False otherwise.
-    """
+    """Returns True if obj is an instance of a_class or its subclass"""
     return isinstance(obj, a_class)
