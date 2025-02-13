7501c0bb59da62f00c7838532a80642f5a0330aa
NourKasmi <nourkasmi@gmail.com>
2025-02-13 23:00:31 +0100

    agin

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,3 @@ def read_file(filename=""):
     """Reads a text file (UTF-8) and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
