3b567e3fa0acf9ad45b38626d5c1aec08da56f3e
NourKasmi <nourkasmi@gmail.com>
2025-02-16 13:15:39 +0100

    everything

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -2,20 +2,17 @@
 from abc import ABC, abstractmethod
 
 
-# Abstract class
 class Animal(ABC):
     @abstractmethod
     def sound(self):
         pass
 
 
-# Subclass Dog implementing the abstract method
 class Dog(Animal):
     def sound(self):
         return "Bark"
 
 
-# Subclass Cat implementing the abstract method
 class Cat(Animal):
     def sound(self):
         return "Meow"
