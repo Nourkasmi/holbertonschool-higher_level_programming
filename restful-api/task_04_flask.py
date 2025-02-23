c8a158bb3f6812c8e32d2d5507839408da31f796
NourKasmi <nourkasmi@gmail.com>
2025-02-23 14:04:16 +0100

    again

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,63 +1,74 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-This is the ``task_04_flask`` module
+Module to develop a Simple API using Python with Flask
 """
-from flask import Flask, jsonify, request
+
+from flask import Flask
+from flask import jsonify
+from flask import request
 
 
-app = Flask(__name__)
-users = {
-    "jane": {
-        "username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"
-    },
-    "john": {
-        "username": "john", "name": "John", "age": 30, "city": "New York"
-    },
-}
+app = Flask(_name_)
+
+users = {}
 
 
-@app.route('/')
+@app.route("/")
+# Define a route for the root URL ("/").
 def home():
-    """Handle root URL"""
+    """
+    Function to handle this route ("/").
+    """
     return "Welcome to the Flask API!"
 
 
-@app.route('/data')
+@app.route("/data")
+# Define a route for the root URL ("/").
 def data():
-    """Display data"""
-    return jsonify(list(users.keys()))
+    """
+    Function to handle data route.
+    """
+    username = list(users.keys())
+    return jsonify(username)
 
 
-@app.route('/status')
-def status():
-    """Display Status"""
+@app.route("/status")
+# Define a route for the root URL ("/").
+def get_status():
+    """
+    Function to return status.
+    """
     return "OK"
 
 
-@app.route('/users/<username>')
-def userInfo(username):
-    """Display specified user"""
-    if username in users:
-        return jsonify(users[username])
-    return jsonify({"error": "User not found"}), 404
+@app.route("/users/<username>")
+# Define a dynamic route for the root URL ("/users/<username>").
+def get_username_obj(username):
+    """
+    Function that returns the full object
+    corresponding to the provided username
+    """
+    user = users.get(username)
+    if user:
+        return jsonify(users)
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=['POST'])
 def add_user():
-    """Add a new user"""
-    userdata = request.get_json()
-    username = userdata.get('username')
+    """
+    Function that adds new user.
+    """
+    new_user = request.get_json()
+    username = new_user.get("username")
+
     if not username:
         return jsonify({"error": "Username is required"}), 400
-    users[username] = {
-        'username': username,
-        'name': userdata.get('name'),
-        'age': userdata.get('age'),
-        'city': userdata.get('city')
-    }
 
-    return jsonify({"message": "User added", "user": users[username]}), 201
+    users[username] = new_user
+    return jsonify({"message": "User added", "user": new_user}), 201
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if _name_ == "_main_":
+    app.run()
