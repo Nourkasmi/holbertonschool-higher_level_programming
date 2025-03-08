316b216c74fc82d49c7dc0df3a66deaa73da8b3a
NourKasmi <nourkasmi@gmail.com>
2025-03-09 00:31:15 +0100

    again

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Script to create the table unique_id with constraints
 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1, UNIQUE,
+    id INT NOT NULL DEFAULT 1, UNIQUE,
     name VARCHAR(256)
 );
