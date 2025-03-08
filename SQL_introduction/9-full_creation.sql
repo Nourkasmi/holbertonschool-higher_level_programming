0513334e9b0554416d07f07b2d3cfcb68e733dd8
NourKasmi <nourkasmi@gmail.com>
2025-03-08 13:02:28 +0100

    again

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- Script to create the table second_table and insert multiple rows
 
 CREATE TABLE IF NOT EXISTS second_table (
-    name VARCHAR(256)?
     id INT,
-    score INT,
-)
+    name VARCHAR(256),
+    score INT
+);
 
-INSERT INTO second_table (name, id, score) VALUES
-     ('John', 1, 10),
-     ('Alex', 2, 3),
-     ('Bob', 3, 14),
-     ('George', 4, 8);
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
