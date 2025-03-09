9555a285608edefb7dd8bf4b6b53138e60b0c2ec
NourKasmi <nourkasmi@gmail.com>
2025-03-09 16:11:33 +0100

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,6 +6,7 @@ Lists all states with a starting with 'N' from the database `hbtn_0e_0_usa`.
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
