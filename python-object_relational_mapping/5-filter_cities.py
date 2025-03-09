ee920d0b2d7450b6304a329a6eb46db4b43d54c9
NourKasmi <nourkasmi@gmail.com>
2025-03-09 16:45:54 +0100

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Lists all cities of a given state from the database hbtn_0e_4_usa.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Retrieves all cities of a given state safely from SQL injection.
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    cur = db.cursor()
+    query = """
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+
+    cur.execute(query, (state_name,))
+    cities = [row[0] for row in cur.fetchall()]
+
+    print(", ".join(cities))
+
+    cur.close()
+    db.close()
